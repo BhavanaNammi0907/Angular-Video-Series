549a4b92c9221e8ef072bdd094c88596fdbbce80
LAPTOP-3Q7N09F8\saibh <sai.bhavana0907@gmail.com>
2024-03-07 22:04:13 -0500

    ngOnInIt, ngOnDestory lifecycle hooks seen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-video-series';
+export class AppComponent implements OnInit, OnDestroy {
+  intervalSub: any;
+
+  title = 'My first angular project';
+
+  ngOnInit(): void {
+      this.intervalSub = setInterval(() => {
+        console.log("Hello from ngOnInIt!");
+      },1000);
+  }
+
+  ngOnDestroy(): void {
+      if(this.intervalSub){
+        clearInterval(this.intervalSub);
+      }
+  }
+
 }
